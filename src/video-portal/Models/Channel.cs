3169649e50bb704cf0d0e08ff737da30833107a4
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2022-10-01 16:55:07 -0300

    Requisito 1

--- src/video-portal/Models/Channel.cs
+++ src/video-portal/Models/Channel.cs
@@ -2,6 +2,11 @@ namespace video_portal.Models
 {
     public class Channel
     {
-
+        public int ChannelId;
+        public string ChannelName;
+        public string? ChannelDescription;
+        public string Url;
+        public Video Videos;
+        public User Owners;
     }
 }
--- src/video-portal/Models/Comment.cs
+++ src/video-portal/Models/Comment.cs
@@ -2,6 +2,9 @@ namespace video_portal.Models
 {
     public class Comment
     {
-
+        public int CommentId;
+        public string CommentText;
+        public int VideoId;
+        public int UserId;
     }
 }
