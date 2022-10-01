f42c4afa849a29cc012d1dfa3bcade7658dd5266
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2022-10-01 17:17:38 -0300

    Requisito 2

--- src/video-portal/Repository/VideoPortalContext.cs
+++ src/video-portal/Repository/VideoPortalContext.cs
@@ -5,6 +5,10 @@ namespace video_portal.Repository;
 
 public class VideoPortalContext : DbContext, IVideoPortalContext
 {
+    public virtual DbSet<Channel> Channels { get; set; }
+    public virtual DbSet<Video> Videos { get; set; }
+    public virtual DbSet<Comment> Comments { get; set; }
+    public virtual DbSet<User> Users { get; set; }
     public VideoPortalContext(DbContextOptions<VideoPortalContext> options) : base(options) { }
     public VideoPortalContext() { }
 }
